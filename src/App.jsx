899a7e5b01bbfb5fd9dabb9c18e1f42fefadd7e4
Jake Nichols <yummyfunnybunny13@gmail.com>
2024-03-13 12:32:42 -0600

    add axios and grab mock data from stuff endpoint to display on screen.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,29 @@
 import './App.css';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function App() {
-  return <>Hello there</>;
+  let [arr, setArr] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get('/stuff')
+      .then((res) => {
+        setArr(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
+  return (
+    <>
+      <h1>Hello there!</h1>
+      {arr.map((num) => {
+        return <p>{num}</p>;
+      })}
+    </>
+  );
 }
 
 export default App;
